63c29383c9e8afb28744d8b4a561b465c32245d6
revanth0480@gmail.com <revanth0480@gmail.com>
2025-09-06 16:00:03 +0530

    Changes to main.py and addition of Procfile

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -28,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 login_manager = LoginManager()
 login_manager.init_app(app)
 ckeditor = CKEditor(app)
@@ -42,7 +43,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
